2832f7488d15bd2dbfadd7a037eaeebda6b373c0
孟政元 <278227739@qq.com>
2017-08-24 19:43:39 +0800

    构建生产版本

--- src/main/entry.js
+++ src/main/entry.js
@@ -1,8 +1,24 @@
 import addNote from './module/addNote';
+import deleteNote from './module/deleteNote';
+import getClass from './module/getClass';
+import getLocation from './module/getLocation';
+import getNotes from './module/getNotes';
+import getStatistics from './module/getStatistics';
+import queryClass from './module/queryClass';
+import releaseClass from './module/releaseClass';
+import setLocation from './module/setLocation';
 
 const yoyo =
     {
         addNote,
+        deleteNote,
+        getClass,
+        getLocation,
+        getNotes,
+        getStatistics,
+        queryClass,
+        releaseClass,
+        setLocation,
     };
 
 export default yoyo;
