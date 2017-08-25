603abce417091a9177fc04691812e614adb9c252
孟政元 <278227739@qq.com>
2017-08-25 22:22:16 +0800

    /getClassContent

--- src/main/module/getClassContent.js
+++ src/main/module/getClassContent.js
@@ -3,6 +3,24 @@ const baseUrl = '//www.vanging.com/yoyo/classes';
 
 export default function(class_id)
 {
-    const url = `${baseUrl}/${class_id}/index.json`;
-    return get(url);
+    return new Promise(function(resolve, reject)
+    {
+        const url = `${baseUrl}/${class_id}/index.json`;
+        get(url)
+            .then(function(result)
+            {
+                try
+                {
+                    result = JSON.parse(result);
+                    resolve(result)
+                }
+                catch(e)
+                {
+                    reject(e);
+                }
+            }, function(err)
+            {
+                reject(err);
+            });
+    });
 }
